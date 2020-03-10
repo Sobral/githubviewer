bec92c201b0726146b35d0ef2ce602def8852f9a
Luciano Sobral <sobral.luciano@gmail.com>
2020-03-10 14:27:59 -0400

    Adding repository route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
 
 import Main from './pages/Main';
+import Repository from './pages/Repository';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
+        <Route path="/repository/:repository" exact component={Repository} />
       </Switch>
     </BrowserRouter>
   );
